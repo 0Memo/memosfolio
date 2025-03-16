a469ae207f026dfeb9acac83b1b2ba8af98fa257
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-03-16 11:27:27 +0100

    new update on vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,17 @@ export default {
     build: {
         rollupOptions: {
             output: {
-                manualChunks: {
-                    three: ['three'], // Split out three.js into a separate chunk
+                manualChunks(id) {
+                    if (id.includes('node_modules/three')) {
+                      return 'three'; // Moves three.js to its own chunk
+                    }
+                    if (id.includes('node_modules')) {
+                      return 'vendor'; // Moves other dependencies to a "vendor" chunk
+                    }
                 },
             },
         },
+        chunkSizeWarningLimit: 1000,
     },
     assetsInclude: ['**/*.mp4', '**/*.glb', '**/*.gltf'],
 };
